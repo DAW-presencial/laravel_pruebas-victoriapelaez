7f6ace15fa370bf88d9c05bf29fe17d92b67898b
Victoria <mpelaez@cifpfbmoll.eu>
2021-12-01 18:02:53 +0100

    añadir breeze

--- proyecto-prueba-laravel/routes/web.php
+++ proyecto-prueba-laravel/routes/web.php
@@ -1,27 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContadorController;
-use App\Http\Controllers\AgendaController;
-use App\Http\Controllers\FormularioValidacionController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
-return view('welcome');
+    return view('welcome');
 });
-Route::get('/visitas', function(){
-    return view('visitas');
-});
-Route::get('/visitasController',[ContadorController::class,'devolverVista']);
-/*Route::get('/visitasController',ContadorController::class);*/
-/*Route::get('/visitasController{name}',[ContadorController::class,'devolverVista']);*/
 
-Route::get('/agenda',[AgendaController::class,'devolverAgenda']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/welcome_viki', function(){
-    return view('welcome_viki');
-});
-Route::get('/{lang}/formulario-validacion',[FormularioValidacionController::class,'devolverFormulario']);
-Route::post('/formulario-validacion',[FormularioValidacionController::class,'devolverFormulario']);
-
-
+require __DIR__.'/auth.php';
