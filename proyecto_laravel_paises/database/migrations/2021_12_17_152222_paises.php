2c8fc42231753075ca2971d866a026da09007cdf
Victoria <mpelaez@cifpfbmoll.eu>
2021-12-17 16:49:06 +0100

    creacion migracion paises

---
+++ proyecto_laravel_paises/database/migrations/2021_12_17_152222_paises.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Paises extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paises', function (Blueprint $table){
+            $table->char('codigoISO3',3)->primary();
+            $table->char('codigoISO2',2)->unique();
+            $table->decimal('cod_numerico',3,0);
+            $table->string('nombre',50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paises');
+    }
+}
