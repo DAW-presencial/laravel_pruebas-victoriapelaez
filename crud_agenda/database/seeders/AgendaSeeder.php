c26c3a86fc648a2e16240b905bef71975f322d70
Victoria <mpelaez@cifpfbmoll.eu>
2022-01-11 13:29:09 +0100

    crud agenda añadir seeder con un contacto random

---
+++ crud_agenda/database/seeders/AgendaSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class AgendaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('agenda')->insert([
+            'nombre' => Str::random(10),
+            'apellido' => Str::random(10),
+            'teléfono'=> Str::random(9),
+            'email' => Str::random(10).'@gmail.com',
+        ]);
+    }
+}
