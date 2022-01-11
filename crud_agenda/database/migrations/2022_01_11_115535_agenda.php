da89cbf1aaaf797c62440802bf2679c7e2cac4f6
Victoria <mpelaez@cifpfbmoll.eu>
2022-01-11 13:07:57 +0100

    crud agenda migration

---
+++ crud_agenda/database/migrations/2022_01_11_115535_agenda.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Agenda extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agenda', function (Blueprint $table){
+            $table->string('nombre',50);
+            $table->string('apellido',50);
+            $table->char('teléfono',9)->unique();
+            $table->string('email',50)->primary();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agenda');
+    }
+}
