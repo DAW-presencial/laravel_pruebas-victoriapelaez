c75ef23ece613274e0c4dfcf7970c04f8f6abc5e
Victoria <mpelaez@cifpfbmoll.eu>
2022-01-27 02:05:24 +0100

    Revert "Revert "1- Migracion tablas""
    This reverts commit 890ee2001b88a3eb7d4f767294e54761419f831c.

---
+++ proyecto-crud-agenda/database/migrations/2022_01_26_233505_create_contactos_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contactos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 50);
+            $table->string('apellido', 50)->nullable();
+            $table->char('telefono', 9)->unique();
+            $table->string('email', 50)->unique();
+            $table->string('foto')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unsignedBigInteger('user_id')->after('email')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+        });
+
+       /* Schema::table('contactos', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable()->after('email');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+        });*/
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contactos');
+    }
+}
