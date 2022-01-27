3f4d1518a98fe51ebb9a5a246e981874adad7f10
Victoria <mpelaez@cifpfbmoll.eu>
2022-01-27 06:31:44 +0100

    creacion vistas edit y create unida a navigation y form

---
+++ proyecto-crud-agenda/resources/views/agenda/create.blade.php
@@ -0,0 +1,6 @@
+@include('layouts.navigation')
+<div class="container">
+    <div class="header text-uppercase p-5">
+        <h3 class="border-bottom border-primary">Crear Contacto</h3></div>
+</div>
+@include('agenda.form')
