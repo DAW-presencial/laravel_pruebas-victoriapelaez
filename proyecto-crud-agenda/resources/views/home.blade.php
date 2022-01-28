51491db5388ee9e4ba9d508770854c230604718d
Victoria <mpelaez@cifpfbmoll.eu>
2022-01-28 05:19:18 +0100

    9 y 10 rutas y vista/ruta home

---
+++ proyecto-crud-agenda/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@include('layouts.head')
+@include('layouts.navigation')
+
+<main>
+    <p class="header text-center m-5">Bienvenido a la Agenda de Contactos.
+    </p>
+</main>
--- proyecto-crud-agenda/routes/web.php
+++ proyecto-crud-agenda/routes/web.php
@@ -1,31 +1,19 @@
 <?php
 
+use App\Http\Controllers\ContactoController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
-Route::get('/form', function (){
-    return view('agenda.form');
-});
-Route::post('/agenda', function (){
-    echo'próximamente será al vista agenda';
-});
+
+require __DIR__ . '/auth.php';
+
+Route::resource('/contactos', ContactoController::class)
+    ->middleware(['auth'])
+    ->parameter('contactos', 'contacto');
