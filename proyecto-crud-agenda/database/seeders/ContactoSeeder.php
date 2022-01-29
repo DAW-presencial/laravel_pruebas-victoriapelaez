351d63222a786cb9c235816f5f1c9a72d812cfb4
Victoria <mpelaez@cifpfbmoll.eu>
2022-01-29 01:32:59 +0100

    mejorar estilo, añadir iconos, añadir datos seeder

--- proyecto-crud-agenda/database/seeders/ContactoSeeder.php
+++ proyecto-crud-agenda/database/seeders/ContactoSeeder.php
@@ -19,29 +19,49 @@ class ContactoSeeder extends Seeder
                 'nombre' => 'Victoria',
                 'apellido' => 'Peláez',
                 'telefono' => '000000000',
-                'email' => 'victoria@gmail.com'
+                'email' => 'victoria@gmail.com',
+                'edad'=>'34',
+                'nacimiento'=>'1988/01/13',
+                'idioma'=>'espanol',
+                'color'=>'rojo'
             ], [
                 'nombre' => 'Patricia',
                 'apellido' => 'Peláez',
                 'telefono' => '111111111',
-                'email' => 'patricia@gmail.com'
+                'email' => 'patricia@gmail.com',
+                'edad'=>'40',
+                'nacimiento'=>'1981/08/10',
+                'idioma'=>'espanol',
+                'color'=>'amarillo'
             ], [
                 'nombre' => 'Ramón',
                 'apellido' => 'Peláez',
                 'telefono' => '222222222',
-                'email' => 'ramon@gmail.com'
+                'email' => 'ramon@gmail.com',
+                'edad'=>'67',
+                'nacimiento'=>'1955/10/13',
+                'idioma'=>'espanol',
+                'color'=>'azul'
             ],
             [
                 'nombre' => 'Joan',
                 'apellido' => 'Rebassa',
                 'telefono' => '333333333',
-                'email' => 'joan@gmail.com'
+                'email' => 'joan@gmail.com',
+                'edad'=>'33',
+                'nacimiento'=>'1988/11/13',
+                'idioma'=>'espanol',
+                'color'=>'verde'
             ],
             [
                 'nombre' => 'Carmen',
                 'apellido' => 'Lozano',
                 'telefono' => '444444444',
-                'email' => 'carmen@gmail.com'
+                'email' => 'carmen@gmail.com',
+                'edad'=>'63',
+                'nacimiento'=>'1959/09/23',
+                'idioma'=>'espanol',
+                'color'=>'rojo'
             ],
         ];
         DB::table('contactos')->insert($contactos);
--- proyecto-crud-agenda/resources/views/contactos/index.blade.php
+++ proyecto-crud-agenda/resources/views/contactos/index.blade.php
@@ -1,21 +1,17 @@
 @include('layouts.head')
 @include('layouts.navigation')
 <div class="container">
-    <div class="row">
-    <h1 class="col w-75 text-center header m-5">@lang('AGENDA CONTACTOS')</h1>
-        <a href="{{ url('contactos/create') }}" class="col align-self-center text-right">
-            <button class="btn btn-info">
-                +@lang('Crear Contacto')
-            </button>
-        </a>
-
-    </div>
+    <h1 class="header m-5 border-bottom">@lang('AGENDA CONTACTOS')</h1>
+    <a href="{{ url('contactos/create') }}" class="">
+        <button class="btn btn-warning justify-center m-3 float-right">
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAZJJREFUSEvtlfEtBUEQxn+vAlSAClABKkAHOkAF6IAK6AAVoAJUgA6ogPxkVyab23d774Q/vEkuudzNzjfzzTezE/7IJn+Eyxz415jvo3oXOADWU0aPwDlwPTbDacBnCbQL4xQ4GQNeA7bSqxT4KVS4Dyyn79vA3azgNWADbgL3wFYRfNq/5jxqwG/AAtBFqRQfA/osNSMVjmOA34HFCnAvK31UG8BeRlPZa5U2ZL+Zge3rbYryAlymd8W10iCumYGNnXvZMk4mk9Wuv6Po7MvOYQjwClhI7652rDwYF4hBywUyLcmY+LdY+zZXq2h/FNhqFVHXHEvhTchKqnPvM9WedflEqqW5SrUBLjoAy+oV0F6a565/tQX05VtS7Vw+F/Pp9opmv10umtlvdIAPVrWi2QFcDlKUx6isyLFSZCbgGSuPNghYiq1WO0qBp4krCmo1965VjZHqeCO1BJLyhwRkxYPu6AgcK2gds48EPPh+jgD2zUcrR6jGYL6P1UJND51nWytrbV2z3xy4maqxjv+P6k9T5Vcf7S6DuQAAAABJRU5ErkJggg==" class="smaller"/>
+        </button>
+    </a>
     <div class="table table-hover table-responsive">
-        <table class="table table-hover table-responsive" style="width: auto; margin: auto auto;">
-            <thead class="thead-light">
+        <table class="table table-hover" style="margin: auto auto;">
+            <thead class="thead-light w-100">
             <tr>
                 <th scope="col" class="text-center">@lang('Acciones')</th>
-
                 <th scope="col" class="text-center">@lang('Id')</th>
                 <th scope="col" class="text-center">@lang('Nombre')</th>
                 <th scope="col" class="text-center">@lang('Apellido')</th>
@@ -32,19 +28,20 @@
             </thead>
             <tbody>
             @foreach($datos as $dato)
-                <tr>
+                <tr class="text-center">
                     <td>
-                        <div class="">
-                            <a href="{{ url('/contactos/'.$dato->id.'/edit') }}" class="col align-self-center text-right">
+                        <div class="row justify-center">
+                            <a href="{{ url('/contactos/'.$dato->id.'/edit') }}" class="m-1">
                                 <button class="btn btn-success">
-                                    @lang('Editar')
+                                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAATpJREFUSEvdlu1xwjAQRB8dUEI6gBJCJQkVUEJIB+kgUEFKSDqADkgJoQIyy0gzQtaX4/M4w/2yPJaedu908oyJYjYRl7sAPwMvwBx4A15LblopFvQ9Au2AdQ5uAQ6hZ+AbWDhgFj4UHEMfHfgrgK8AjW9iCDi2V7ZKoeIBOLln5XprBU7l9AeQOln9CSwdzEyxKnbjFlVONVY1KwQX2EP3gDbZib5Wy8qnAKqcHt3icVVnoZrfB5yDah2d3YPLrcZFaB9wCapC+mixN/S7RXEJqlyqkKS4SamH18CjQGtWjwYtgUeFlsCXxJHRqz/ntLVzeXDY7sygLYo92BTaAk51u2pzSE3qa3X8vQm0pLhzjbkLQBeCSdQaiAkktci/A/vjZKW4IzCneDJwqriGqDf75xqyievcyYrrF4jbVR89q4r1AAAAAElFTkSuQmCC"/>
                                 </button>
                             </a>
-                            <form action="{{ url('/contactos/'.$dato->id) }}" method="post" class="text-center m-2">
+                            <form action="{{ url('/contactos/'.$dato->id) }}" method="post" class="m-1">
                                 @csrf
                                 {{ method_field('DELETE') }}
-                                <button class="btn btn-danger" onclick="return confirm(@lang('¿Deseas borrar el contacto?')">
-                                    @lang('Borrar')
+                                <button class="btn btn-danger"
+                                        onclick="return confirm(@lang('¿Deseas borrar el contacto?')">
+                                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAARdJREFUSEvtln0NwkAMxX9zgAOQAAoABYACcAY4AAWAApAADpBAHlnINm7XXrZkkKz/LOm1ff14611GR5J1hIsXeAAsgJGR6BPYA/pGxQt8B4ZWsPz8BkwsWw/wGLhagSrnAlYCteIBngGnPMIcONdE89q93X8GWORZVyqSbpPrdoDmHRLLTqT7+FYrLrYrcaymeWlMPwNcTFskmgIXQJ1IEdM3Ri7TOZKJ6ZsKLJJpkTwAEU0S0rUOHAro1ZUalFqxF6Sv+IuHfautBdKTSx3ydqHRf6wtpetP11txc1V1rf/H3oviv4G9VYbsjsAydBBbIJrntgkqsAIOqcCy19M2mLEjIc257kXqemU6MNJNPM/b9KgOjxfTSocfMilmtwAAAABJRU5ErkJggg=="/>
                                 </button>
                             </form>
                         </div>
