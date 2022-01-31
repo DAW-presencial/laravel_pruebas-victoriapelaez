4056eb3ff655794e8fac13dbf8fd708303c2b146
Victoria <mpelaez@cifpfbmoll.eu>
2022-01-31 12:03:06 +0100

    11-modelo contacto y actualizar modelo user

---
+++ proyecto-crud-agenda/app/Models/Contacto.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Contacto extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'contactos';
+
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'teléfono',
+        'email',
+        'edad',
+        'nacimiento',
+        'idioma',
+        'descripcion',
+        'color',
+        'privacidad',
+        'user_id'
+    ];
+    protected $guarded = [
+        'id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
