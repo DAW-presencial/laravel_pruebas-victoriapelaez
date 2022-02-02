b419cbd585fe2a97e0117129a9fb6a7697a1a353
Victoria <mpelaez@cifpfbmoll.eu>
2022-02-02 13:53:06 +0100

    arreglo vistas, formulario para old values

--- proyecto-crud-agenda/resources/views/contactos/create.blade.php
+++ proyecto-crud-agenda/resources/views/contactos/create.blade.php
@@ -10,7 +10,105 @@
             <form action="{{ url('/contactos') }}" method="post" class=" well form-horizontal"
                   enctype="multipart/form-data">
                 @csrf
-                @include('contactos.form')
+                <fieldset>
+                    <legend class="text-center header"></legend>
+
+                    <div class="form-group ">
+                        <i class="fa fa-user bigicon"></i>
+                        <label for="nombre" class="col-sm-2 col-form-label">@lang('Nombre')</label>
+                        <input type="text" name="nombre"
+                               value="{{ isset($datos->nombre)?$datos->nombre:old('nombre') }}"
+                               id="nombre" placeholder="Nombre" class="form-control"/><br>
+                    </div>
+
+                    <div class="form-group">
+                        <i class="fa fa-user bigicon"></i>
+                        <label for="apellido" class="col-sm-2 col-form-label">@lang('Apellido')</label>
+                        <input type="text" name="apellido"
+                               value="{{ isset($datos->apellido)?$datos->apellido:old('apellido') }}"
+                               id="apellido" placeholder="Apellido" class="form-control"/><br>
+                    </div>
+
+                    <div class="form-group">
+                        <i class="fa fa-phone-square bigicon"></i>
+                        <label for="telefono" class="col-sm-2 col-form-label">@lang('Teléfono')</label>
+                        <input type="text" name="telefono"
+                               value="{{ isset($datos->telefono)?$datos->telefono:old('telefono') }}"
+                               id="telefono" placeholder="Teléfono" class="form-control"/><br>
+                    </div>
+
+                    <div class="form-group">
+                        <i class="fa fa-envelope-o bigicon"></i>
+                        <label for="email" class="col-sm-2 col-form-label">@lang('Email')</label>
+                        <input type="text" name="email"
+                               value="{{ isset($datos->email)?$datos->email:old('email') }}" id="email"
+                               placeholder="Email" class="form-control"/><br>
+                    </div>
+
+                    <div class="form-group">
+                        <i class="fa fa-user bigicon"></i>
+                        <label for="edad" class="col-sm-2 col-form-label">@lang('Edad')</label>
+                        <input type="number" name="edad" value="{{ old('edad', $datos->edad ?? '') }}"
+                               id="edad"
+                               class="form-control"/><br>
+                    </div>
+
+                    <div class="form-group">
+                        <i class="fa fa-user bigicon"></i>
+                        <label for="nacimiento" class="col-sm-2 col-form-label">@lang('Fecha de Nacimiento')</label>
+                        <input type="date" name="nacimiento"
+                               value="{{ isset($datos->nacimiento)?$datos->nacimiento:old('nacimiento') }}"
+                               id="nacimiento"
+                               class="form-control"/><br>
+                    </div>
+
+                    <i class="fa fa-pencil-square-o bigicon"></i>
+                    @lang('Idioma'):<br><br>
+                    <div class="form-check">
+                        <label for="idioma" for="idioma">
+                            <input class="form-check-input" type="radio"
+                                   name="idioma" value="espanol" {{ old('idioma') == 'espanol' ? 'checked' : '' }}/> @lang('Español')
+                            <br>
+                            <input class="form-check-input" type="radio"
+                                   name="idioma" value="ingles" {{ old('idioma') == 'ingles' ? 'checked' : '' }}/> @lang("Inglés")
+                        </label>
+
+                    </div>
+                    <br>
+
+                    <div class="form-group">
+                        <i class="fa fa-pencil-square-o bigicon"></i>
+                        <label for="descripcion" class="col-form-label">@lang('Escribe una breve descripción de ti'):</label><br>
+                        <textarea id="descripcion" name="descripcion" class="form-control" rows="4" cols="50">{{ isset($datos->descripcion)?$datos->descripcion:old('descripcion') }}
+        </textarea><br>
+                    </div>
+
+                    <div class="d-flex w-25">
+                        <i class="fa fa-pencil-square-o bigicon align-self-center"></i>
+                        <label for="color" class="col col-form-label">@lang('Elije un color favorito'):</label><br>
+                        <div class="form-check">
+                            <select name="color" id="color" class="form-check-input">
+                                <option value="rojo" @if (old('color') === 'rojo') selected @endif>@lang('Rojo')</option>
+                                <option value="azul" @if (old('color') === 'azul') selected @endif>@lang('Azul')</option>
+                                <option value="verde" @if (old('color') === 'verde') selected @endif>@lang('Verde')</option>
+                                <option value="amarillo" @if (old('color') === 'amarillo') selected @endif>@lang('Amarillo')</option>
+                            </select><br>
+                        </div>
+                    </div>
+                    <br><br>
+
+                    <div class="form-check">
+                        <label for="privacidad" class="col col-form-label">
+                            <input type="checkbox" name="privacidad" id="privacidad" class="form-check-input" value="1"
+                                   @if(isset($datos->privacidad)) @if($datos->privacidad=="1") checked @endif @endif >@lang('Acepto los términos de privacidad')
+                        </label><br>
+
+                    </div>
+                    <br>
+
+                    <button type="submit" class="btn btn-primary mb-2">@lang('Aceptar')</button>
+
+                </fieldset>
             </form>
         </div>
     </div>
--- proyecto-crud-agenda/resources/views/contactos/index.blade.php
+++ proyecto-crud-agenda/resources/views/contactos/index.blade.php
@@ -2,28 +2,34 @@
 @include('layouts.navigation')
 <div class="container">
     <h1 class="header m-5 border-bottom">@lang('AGENDA CONTACTOS')</h1>
-    @if ((Request::route()->getName()=='contactos.create'))
-    <a href="{{ url('contactos/create') }}">
-        <button class="btn btn-warning justify-center m-3 float-right"
-                @if (Gate::allows('create-contacto')) enabled @else disabled @endif>
-            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAZJJREFUSEvtlfEtBUEQxn+vAlSAClABKkAHOkAF6IAK6AAVoAJUgA6ogPxkVyab23d774Q/vEkuudzNzjfzzTezE/7IJn+Eyxz415jvo3oXOADWU0aPwDlwPTbDacBnCbQL4xQ4GQNeA7bSqxT4KVS4Dyyn79vA3azgNWADbgL3wFYRfNq/5jxqwG/AAtBFqRQfA/osNSMVjmOA34HFCnAvK31UG8BeRlPZa5U2ZL+Zge3rbYryAlymd8W10iCumYGNnXvZMk4mk9Wuv6Po7MvOYQjwClhI7652rDwYF4hBywUyLcmY+LdY+zZXq2h/FNhqFVHXHEvhTchKqnPvM9WedflEqqW5SrUBLjoAy+oV0F6a565/tQX05VtS7Vw+F/Pp9opmv10umtlvdIAPVrWi2QFcDlKUx6isyLFSZCbgGSuPNghYiq1WO0qBp4krCmo1965VjZHqeCO1BJLyhwRkxYPu6AgcK2gds48EPPh+jgD2zUcrR6jGYL6P1UJND51nWytrbV2z3xy4maqxjv+P6k9T5Vcf7S6DuQAAAABJRU5ErkJggg==" class="smaller"/>
-        </button>
-        @endif
-    </a>
-    {{--@if (auth()->user()->role ==='admin')
-        <a href="{{ url('contactos/create') }}">
-            <button class="btn btn-warning justify-center m-3 float-right">
-                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAZJJREFUSEvtlfEtBUEQxn+vAlSAClABKkAHOkAF6IAK6AAVoAJUgA6ogPxkVyab23d774Q/vEkuudzNzjfzzTezE/7IJn+Eyxz415jvo3oXOADWU0aPwDlwPTbDacBnCbQL4xQ4GQNeA7bSqxT4KVS4Dyyn79vA3azgNWADbgL3wFYRfNq/5jxqwG/AAtBFqRQfA/osNSMVjmOA34HFCnAvK31UG8BeRlPZa5U2ZL+Zge3rbYryAlymd8W10iCumYGNnXvZMk4mk9Wuv6Po7MvOYQjwClhI7652rDwYF4hBywUyLcmY+LdY+zZXq2h/FNhqFVHXHEvhTchKqnPvM9WedflEqqW5SrUBLjoAy+oV0F6a565/tQX05VtS7Vw+F/Pp9opmv10umtlvdIAPVrWi2QFcDlKUx6isyLFSZCbgGSuPNghYiq1WO0qBp4krCmo1965VjZHqeCO1BJLyhwRkxYPu6AgcK2gds48EPPh+jgD2zUcrR6jGYL6P1UJND51nWytrbV2z3xy4maqxjv+P6k9T5Vcf7S6DuQAAAABJRU5ErkJggg==" class="smaller"/>
+
+    @if ( Gate::allows('create-contacto'))
+        <form action="{{ url('contactos/create') }}">
+            <button type="submit" class="btn btn-warning justify-center m-3 float-right" enabled>
+                <img
+                    src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAZJJREFUSEvtlfEtBUEQxn+vAlSAClABKkAHOkAF6IAK6AAVoAJUgA6ogPxkVyab23d774Q/vEkuudzNzjfzzTezE/7IJn+Eyxz415jvo3oXOADWU0aPwDlwPTbDacBnCbQL4xQ4GQNeA7bSqxT4KVS4Dyyn79vA3azgNWADbgL3wFYRfNq/5jxqwG/AAtBFqRQfA/osNSMVjmOA34HFCnAvK31UG8BeRlPZa5U2ZL+Zge3rbYryAlymd8W10iCumYGNnXvZMk4mk9Wuv6Po7MvOYQjwClhI7652rDwYF4hBywUyLcmY+LdY+zZXq2h/FNhqFVHXHEvhTchKqnPvM9WedflEqqW5SrUBLjoAy+oV0F6a565/tQX05VtS7Vw+F/Pp9opmv10umtlvdIAPVrWi2QFcDlKUx6isyLFSZCbgGSuPNghYiq1WO0qBp4krCmo1965VjZHqeCO1BJLyhwRkxYPu6AgcK2gds48EPPh+jgD2zUcrR6jGYL6P1UJND51nWytrbV2z3xy4maqxjv+P6k9T5Vcf7S6DuQAAAABJRU5ErkJggg=="
+                    class="smaller"/>
             </button>
-            @endif
-        </a>--}}
+        </form>
+    @else
+        <form action="{{ url('contactos/create') }}">
+            <button type="submit" class="btn btn-warning justify-center m-3 float-right" disabled>
+                <img
+                    src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAZJJREFUSEvtlfEtBUEQxn+vAlSAClABKkAHOkAF6IAK6AAVoAJUgA6ogPxkVyab23d774Q/vEkuudzNzjfzzTezE/7IJn+Eyxz415jvo3oXOADWU0aPwDlwPTbDacBnCbQL4xQ4GQNeA7bSqxT4KVS4Dyyn79vA3azgNWADbgL3wFYRfNq/5jxqwG/AAtBFqRQfA/osNSMVjmOA34HFCnAvK31UG8BeRlPZa5U2ZL+Zge3rbYryAlymd8W10iCumYGNnXvZMk4mk9Wuv6Po7MvOYQjwClhI7652rDwYF4hBywUyLcmY+LdY+zZXq2h/FNhqFVHXHEvhTchKqnPvM9WedflEqqW5SrUBLjoAy+oV0F6a565/tQX05VtS7Vw+F/Pp9opmv10umtlvdIAPVrWi2QFcDlKUx6isyLFSZCbgGSuPNghYiq1WO0qBp4krCmo1965VjZHqeCO1BJLyhwRkxYPu6AgcK2gds48EPPh+jgD2zUcrR6jGYL6P1UJND51nWytrbV2z3xy4maqxjv+P6k9T5Vcf7S6DuQAAAABJRU5ErkJggg=="
+                    class="smaller"/>
+            </button>
+        </form>
+    @endif
+
     <div class="table table-hover table-responsive">
         <table class="table table-hover" style="margin: auto auto;">
             <thead class="thead-light w-100">
             <tr>
-                @can('crear-contacto')
-                <th scope="col" class="text-center">@lang('Acciones')</th>
-                @endcan
+                @if ( Gate::allows('update-contacto'))
+                    <th scope="col" class="text-center" enabled>@lang('Acciones')</th>
+                @else
+                    <th scope="col" class="text-center" disabled>@lang('Acciones')</th>
+                @endif
                 <th scope="col" class="text-center">@lang('Id')</th>
                 <th scope="col" class="text-center">@lang('Nombre')</th>
                 <th scope="col" class="text-center">@lang('Apellido')</th>
@@ -34,32 +40,48 @@
                 <th scope="col" class="text-center">@lang('Idioma')</th>
                 {{--<th scope="col">Privacidad</th>
                 <th scope="col">Descripción</th>--}}
-                <th scope="col">@lang('Color')</th>
-
+                <th scope="col" class="text-center">@lang('Color')</th>
             </tr>
             </thead>
             <tbody>
             @foreach($datos as $dato)
                 <tr class="text-center">
-                    @can('update-contacto')
                     <td>
                         <div class="row justify-center">
-                            <a href="{{ url('/contactos/'.$dato->id.'/edit') }}" class="m-1">
-                                <button class="btn btn-success">
-                                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAATpJREFUSEvdlu1xwjAQRB8dUEI6gBJCJQkVUEJIB+kgUEFKSDqADkgJoQIyy0gzQtaX4/M4w/2yPJaedu908oyJYjYRl7sAPwMvwBx4A15LblopFvQ9Au2AdQ5uAQ6hZ+AbWDhgFj4UHEMfHfgrgK8AjW9iCDi2V7ZKoeIBOLln5XprBU7l9AeQOln9CSwdzEyxKnbjFlVONVY1KwQX2EP3gDbZib5Wy8qnAKqcHt3icVVnoZrfB5yDah2d3YPLrcZFaB9wCapC+mixN/S7RXEJqlyqkKS4SamH18CjQGtWjwYtgUeFlsCXxJHRqz/ntLVzeXDY7sygLYo92BTaAk51u2pzSE3qa3X8vQm0pLhzjbkLQBeCSdQaiAkktci/A/vjZKW4IzCneDJwqriGqDf75xqyievcyYrrF4jbVR89q4r1AAAAAElFTkSuQmCC"/>
-                                </button>
-                            </a>
-                            @endcan
-                            @can('delete-contacto')
-                            <form action="{{ url('/contactos/'.$dato->id) }}" method="post" class="m-1">
-                                @csrf
-                                {{ method_field('DELETE') }}
-                                <button class="btn btn-danger"
-                                        onclick="return confirm(@lang('¿Deseas borrar el contacto?')">
-                                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAARdJREFUSEvtln0NwkAMxX9zgAOQAAoABYACcAY4AAWAApAADpBAHlnINm7XXrZkkKz/LOm1ff14611GR5J1hIsXeAAsgJGR6BPYA/pGxQt8B4ZWsPz8BkwsWw/wGLhagSrnAlYCteIBngGnPMIcONdE89q93X8GWORZVyqSbpPrdoDmHRLLTqT7+FYrLrYrcaymeWlMPwNcTFskmgIXQJ1IEdM3Ri7TOZKJ6ZsKLJJpkTwAEU0S0rUOHAro1ZUalFqxF6Sv+IuHfautBdKTSx3ydqHRf6wtpetP11txc1V1rf/H3oviv4G9VYbsjsAydBBbIJrntgkqsAIOqcCy19M2mLEjIc257kXqemU6MNJNPM/b9KgOjxfTSocfMilmtwAAAABJRU5ErkJggg=="/>
-                                </button>
-                            </form>
-                                @endcan
+                            @if ( Gate::allows('update-contacto'))
+                                <form action="{{ url('/contactos/'.$dato->id.'/edit') }}" class="m-1">
+                                    <button type="submit" class="btn btn-success" enabled>
+                                        <img
+                                            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAATpJREFUSEvdlu1xwjAQRB8dUEI6gBJCJQkVUEJIB+kgUEFKSDqADkgJoQIyy0gzQtaX4/M4w/2yPJaedu908oyJYjYRl7sAPwMvwBx4A15LblopFvQ9Au2AdQ5uAQ6hZ+AbWDhgFj4UHEMfHfgrgK8AjW9iCDi2V7ZKoeIBOLln5XprBU7l9AeQOln9CSwdzEyxKnbjFlVONVY1KwQX2EP3gDbZib5Wy8qnAKqcHt3icVVnoZrfB5yDah2d3YPLrcZFaB9wCapC+mixN/S7RXEJqlyqkKS4SamH18CjQGtWjwYtgUeFlsCXxJHRqz/ntLVzeXDY7sygLYo92BTaAk51u2pzSE3qa3X8vQm0pLhzjbkLQBeCSdQaiAkktci/A/vjZKW4IzCneDJwqriGqDf75xqyievcyYrrF4jbVR89q4r1AAAAAElFTkSuQmCC"/>
+                                    </button>
+                                </form>
+                            @else
+                                <form action="{{ url('/contactos/'.$dato->id.'/edit') }} " class="m-1">
+                                    <button type="submit" class="btn btn-success" disabled>
+                                        <img
+                                            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAATpJREFUSEvdlu1xwjAQRB8dUEI6gBJCJQkVUEJIB+kgUEFKSDqADkgJoQIyy0gzQtaX4/M4w/2yPJaedu908oyJYjYRl7sAPwMvwBx4A15LblopFvQ9Au2AdQ5uAQ6hZ+AbWDhgFj4UHEMfHfgrgK8AjW9iCDi2V7ZKoeIBOLln5XprBU7l9AeQOln9CSwdzEyxKnbjFlVONVY1KwQX2EP3gDbZib5Wy8qnAKqcHt3icVVnoZrfB5yDah2d3YPLrcZFaB9wCapC+mixN/S7RXEJqlyqkKS4SamH18CjQGtWjwYtgUeFlsCXxJHRqz/ntLVzeXDY7sygLYo92BTaAk51u2pzSE3qa3X8vQm0pLhzjbkLQBeCSdQaiAkktci/A/vjZKW4IzCneDJwqriGqDf75xqyievcyYrrF4jbVR89q4r1AAAAAElFTkSuQmCC"/>
+                                    </button>
+                                </form>
+                            @endif
+
+                            @if ( Gate::allows('delete-contacto'))
+                                <form action="{{ url('/contactos/'.$dato->id) }}" method="POST" class="m-1">
+                                    @csrf
+                                    {{ method_field('DELETE') }}
+                                    <button type="submit" class="btn btn-danger"
+                                             enabled>
+                                        <img
+                                            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAARdJREFUSEvtln0NwkAMxX9zgAOQAAoABYACcAY4AAWAApAADpBAHlnINm7XXrZkkKz/LOm1ff14611GR5J1hIsXeAAsgJGR6BPYA/pGxQt8B4ZWsPz8BkwsWw/wGLhagSrnAlYCteIBngGnPMIcONdE89q93X8GWORZVyqSbpPrdoDmHRLLTqT7+FYrLrYrcaymeWlMPwNcTFskmgIXQJ1IEdM3Ri7TOZKJ6ZsKLJJpkTwAEU0S0rUOHAro1ZUalFqxF6Sv+IuHfautBdKTSx3ydqHRf6wtpetP11txc1V1rf/H3oviv4G9VYbsjsAydBBbIJrntgkqsAIOqcCy19M2mLEjIc257kXqemU6MNJNPM/b9KgOjxfTSocfMilmtwAAAABJRU5ErkJggg=="/>
+                                    </button>
+                                </form>
+                            @else
+                                <form action="{{ url('/contactos/'.$dato->id) }}" method="POST" class="m-1">
+                                    <button type="submit" class="btn btn-danger" disabled>
+                                        <img
+                                            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAARdJREFUSEvtln0NwkAMxX9zgAOQAAoABYACcAY4AAWAApAADpBAHlnINm7XXrZkkKz/LOm1ff14611GR5J1hIsXeAAsgJGR6BPYA/pGxQt8B4ZWsPz8BkwsWw/wGLhagSrnAlYCteIBngGnPMIcONdE89q93X8GWORZVyqSbpPrdoDmHRLLTqT7+FYrLrYrcaymeWlMPwNcTFskmgIXQJ1IEdM3Ri7TOZKJ6ZsKLJJpkTwAEU0S0rUOHAro1ZUalFqxF6Sv+IuHfautBdKTSx3ydqHRf6wtpetP11txc1V1rf/H3oviv4G9VYbsjsAydBBbIJrntgkqsAIOqcCy19M2mLEjIc257kXqemU6MNJNPM/b9KgOjxfTSocfMilmtwAAAABJRU5ErkJggg=="/>
+                                    </button>
+                                </form>
+                            @endif
                         </div>
                     </td>
                     <td>{{$dato->id}}</td>
@@ -73,7 +95,6 @@
                     {{--<td>{{$dato->deportes}}</td>
                     <td>{{$dato->descripcion}}</td>--}}
                     <td>{{$dato->color}}</td>
-
                 </tr>
             @endforeach
             </tbody>
